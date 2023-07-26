35f3f1a610b3954db6500f1ef9f79ae286e8fb93
AshutoshM18 <gouravmaurya18@gmail.com>
2023-07-26 17:40:44 +0530

    Demo Renting Project

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -3,9 +3,9 @@
 import Container from "../Container";
 import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
 import { GiBarn, GiBoatFishing, GiCactus, GiCastle, GiCaveEntrance, GiForestCamp, GiIsland, GiWindmill } from "react-icons/gi";
-import { FaSkiing } from "react-icons/Fa";
-import { IoDiamond } from "react-icons/Io5";
-import { BsSnow } from "react-icons/Bs";
+import { FaSkiing } from "react-icons/fa";
+import { IoDiamond } from "react-icons/io5";
+import { BsSnow } from "react-icons/bs";
 import { MdOutlineVilla } from "react-icons/md";
 import CategoryBox from "../CategoryBox";
 import { usePathname, useSearchParams } from "next/navigation";
